9d3d29f1eea24dd0f7c511e414df197d0bb882e4
David Hansen <dracozlair@gmail.com>
2023-11-03 10:44:36 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const db = CyclicDB(process.env.CYCLIC_DB)
+let contentCollection = db.collection('content')
+
+router.get("/json", async (req, res, next) => {
+  let content = await contentCollection.get("content")
+
+  if (content == null) {
+    res.json({
+      status: "fail",
+      message: "Content is empty, add some content first"
+    });
+  } else {
+    let contentValue = content.props.value
+    res.json({
+      status: "success",
+      result: contentValue,
+    });
+  }
+});
+
+router.post("/json", async (req, res, next) => {
+  const { content } = req.body;
+  if (content == null) {
+    res.status(400).send("Content not provided");
+    return;
+  }
+  await contentCollection.set("content", {
+    value: content
+  })
+  res.json({
+    status: "success",
+    content: content,
+  });
+});
+
+module.exports = router;
